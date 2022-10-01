71a6bf6ee851d8da5b12af0e4d69184ae5d6a3ba
hugo <hugo@moniker.net>
2022-10-01 13:20:15 +1000

    added name to User in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 16 }
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: true
 end
